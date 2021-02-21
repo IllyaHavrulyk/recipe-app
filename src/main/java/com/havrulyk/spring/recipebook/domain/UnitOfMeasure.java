e40e723dd5a512c792003708fadbad4bd9ada308
Illya Havrulyk <peoso595@gmail.com>
2021-02-21 15:52:38 +0200

    Added integrational tests for unit of measure

--- src/main/java/com/havrulyk/spring/recipebook/domain/UnitOfMeasure.java
+++ src/main/java/com/havrulyk/spring/recipebook/domain/UnitOfMeasure.java
@@ -2,8 +2,12 @@ package com.havrulyk.spring.recipebook.domain;
 
 import javax.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @Entity
 public class UnitOfMeasure {
 
--- src/test/java/com/havrulyk/spring/recipebook/controllers/IndexControllerTest.java
+++ src/test/java/com/havrulyk/spring/recipebook/controllers/IndexControllerTest.java
@@ -1,21 +1,22 @@
 package com.havrulyk.spring.recipebook.controllers;
 
-import com.havrulyk.spring.recipebook.repositories.RecipeRepository;
-import com.havrulyk.spring.recipebook.services.RecipeService;
+import com.havrulyk.spring.recipebook.domain.Recipe;
 import com.havrulyk.spring.recipebook.services.RecipeServiceImpl;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anySet;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -34,9 +35,31 @@ class IndexControllerTest {
     }
 
     @Test
+    public void mockMvcTest() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+//        mockMvc.perform(MockServerHttpRequest.get("/").build())
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("index"));
+    }
+
+    @Test
     void getIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> indexController = argumentCaptor.getValue();
+        assertEquals(1, indexController.size());
+
     }
 }
